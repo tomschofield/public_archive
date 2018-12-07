c5276f444fc489298f7693c63ff12f097ffd8a34
Tom Schofield <tomschofieldart@gmail.com>
2018-12-07 14:33:29 +0000

    Update usb_listen_transfer_till_full.py

--- python/usb_listen_transfer_till_full.py
+++ python/usb_listen_transfer_till_full.py
@@ -120,7 +120,7 @@ def listen(image_dir,mount_directory,green_pin,red_pin, stick_ripped_out):
 		#this is a hacky way of going through sub dirs of a known mount point (/media) and find one that starts in sd and is big
 		for x in os.walk(mount_directory):
 			if(len(x[0].split("/")) == 3):
-				if(x[0].split("/")[2][:2]=="sd"):
+				if(x[0].split("/")[2][:2]=="pi"):
 					if device_count < 1:
 						print "device name ", x[0]
 						#this line is hack to remove non-existing virtual disk e.g. /media/sda1 (these should return a size which is the available space on the pi)
